9a2433f400c29ccc3b121c44221f50406daf6d10
Rosen <rosenclever@gmail.com>
2019-05-22 11:09:32 -0300

    fatorial

--- prova_1bim_2019/prova1bim.php
+++ prova_1bim_2019/prova1bim.php
@@ -17,9 +17,12 @@
 		echo "<br/>O sexo cadastrado é Masculino!";
 	}
 	foreach ($conhecimentos as $conhecimento => $value) {
-		echo "<br/>$value";
+		if ($value != "") {
+			echo "<br/>$value";
+		}
+		
 	}
-	if ($operacao = "primo") {
+	if ($operacao == "primo") {
 		echo "<br/>A idade informada corresponde a um número ";
 		for ($i=2; $i <= $idade/2 ; $i++) { 
 			if ($idade%$i==0) {
@@ -31,5 +34,22 @@
 		if ($primo) {
 			echo "primo";
 		}
+	}
+	if ($operacao == "par" ) {
+		if ($idade%2==0) {
+			echo "<br/>A idade informada corresponde a um número PAR!";
+		}
+		else{
+			echo "<br/>A idade informada corresponde a um número ÍMPAR!";
+		}
+		
+	}
+	if ($operacao == "fatorial") {
+		echo "<br/>O fatorial da idade é ";
+		$fat = 1;
+		for ($i=1; $i <= $idade ; $i++) { 
+			$fat *= $i;
+		}
+		echo "$fat";
 	}
 ?>
